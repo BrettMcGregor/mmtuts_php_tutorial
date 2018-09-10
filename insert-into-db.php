dd3b99f10beb2776a4b1c9a01820cc8da1f341bf
Brett McGregor <brett.w.mcgregor@gmail.com>
2018-09-10 11:39:15 +1000

    sign up form validation features

--- insert-into-db.php
+++ insert-into-db.php
@@ -1,34 +1,102 @@
-<?php
-    include_once 'db-connect.php';
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Insert into DB</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <!-- form action is the code that will be run when submit button is clicked -->
+<h2>Signup</h2>
 <form action="signup.php" method="POST">
-    <input type="text" name="first" placeholder="Firstname">
-    <br>
-    <input type="text" name="last" placeholder="Lastname">
-    <br>
-    <input type="text" name="email" placeholder="Email">
-    <br>
-    <input type="text" name="uid" placeholder="Username">
-    <br>
+    <?php
+        // if the form is submitted, use the GET method to return the form data back to the form
+        if (isset($_GET['first'])) {
+        $first = $_GET['first'];   
+        // Note the syntax for placing the variable into the value attribute
+        echo '<input type="text" name="first" placeholder="Firstname" value="'.$first.'">';
+        } 
+        else {
+        echo '<input type="text" name="first" placeholder="Firstname">';
+        }
+
+        if (isset($_GET['last'])) {
+        $last = $_GET['last'];   
+        echo '<input type="text" name="last" placeholder="Lastname" value="'.$last.'">';
+        } 
+        else {
+        echo '<input type="text" name="last" placeholder="Lastname">';
+        }
+    
+        if (isset($_GET['email'])) {
+        $email = $_GET['email'];   
+        echo '<input type="text" name="email" placeholder="Email" value="'.$email.'">';
+        } 
+        else {
+        echo '<input type="text" name="email" placeholder="Email">';
+        }
+
+        if (isset($_GET['uid'])) {
+        $uid = $_GET['uid'];   
+        echo '<input type="text" name="uid" placeholder="Username" value="'.$uid.'">';
+        } 
+        else {
+        echo '<input type="text" name="uid" placeholder="Username">';
+        }
+    ?>
+    <!-- Chosen not to return the password to the form -->
     <input type="password" name="pwd" placeholder="Password">
     <br>
     <button type="submit" name="submit">Submit</button>
 </form>
 
+<?php
+/*
+ $fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+ 
+ if (strpos($fullUrl, "signup=empty") == TRUE) {
+    echo "<p class='error'>You did not complete all fields</p>";
+    exit();
+ }
+ elseif (strpos($fullUrl, "signup=char") == TRUE) {
+    echo "<p class='error'>You used invalid characters</p>";
+    exit();
+ }
+ elseif (strpos($fullUrl, "signup=invalid-email") == TRUE) {
+    echo "<p class='error'>You used an invalid email</p>";
+    exit();
+ }
+ elseif (strpos($fullUrl, "signup=success") == TRUE) {
+    echo "<p class='success'>You have been signed up!</p>";
+    exit();
+ }
+ */
 
+//  alternative approach where user-entered data is retained in the form fields
+
+if (!isset($_GET['signup'])) {
+    exit();
+} else {
+    $signupCheck = $_GET['signup'];
+
+    if ($signupCheck == "empty") {
+        echo "<p class='error'>You did not complete all fields</p>";
+        exit();
+    }
+    elseif ($signupCheck == "char") {
+        echo "<p class='error'>You used invalid characters</p>";
+        exit();
+    }
+    elseif ($signupCheck == "invalid-email") {
+        echo "<p class='error'>You used an invalid email</p>";
+        exit();
+    }
+    elseif ($signupCheck == "success") {
+        echo "<p class='success'>You have been signed up!</p>";
+        exit();
+    }
+}
+?>
 
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php
+/*
     include_once 'db-connect.php';
 
     // User inputs to the form are saved to variables
@@ -22,4 +23,60 @@
         mysqli_stmt_execute($stmt);
     }
     header("Location: index.php?signup=success")
+*/
+
+// Error Handling example (as above but with some error handlers)
+
+if (isset($_POST['submit'])) {
+    // refer the database connection
+    include_once 'db-connect.php';
+
+    // get the user-entered data from the form
+    $first = $_POST['first'];
+    $last = $_POST['last'];
+    $email = $_POST['email'];
+    $uid = $_POST['uid'];
+    $pwd = $_POST['pwd'];
+
+    // check all fields contain data
+    if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
+        // if any empty field, redirect to index
+        header("Location: insert-into-db.php?signup=empty");
+        exit();
+        } else {
+            // Check input characters are valid
+            if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
+            header("Location: insert-into-db.php?signup=char&first=$first&last=$last&email=$email&uid=$uid");
+            exit();
+            } else {
+            // validate email
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    header("Location: insert-into-db.php?signup=invalid-email&first=$first&last=$last&uid=$uid");
+                    exit(); 
+                } else {
+                    // Insert into the database using the variables above
+                    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?);";
+                    $stmt = mysqli_stmt_init($conn);
+                    // handle error with statement prepare
+                    if (!mysqli_stmt_prepare($stmt, $sql)) {
+                        echo "SQL Error";
+                    } 
+                    else {
+                        // Insert new user into database
+                        // Bind parameters to the placeholder(s)
+                        // "sssss" placeholders in the function here, correspond to the placeholders in the sql query
+                        mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
+                        // Run parameters inside database
+                        mysqli_stmt_execute($stmt);
+                        header("Location: insert-into-db.php?signup=success");
+                        exit();
+                    }   
+                }
+            }
+        }
+} else {
+    header("Location: insert-into-db.php?signup=error");
+    exit();
+}
+
 ?>
